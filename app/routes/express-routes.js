c6682abc131efc89d87e5c7178f5f69ac4fd21a5
Gabrielle <gabivk@gmail.com>
2017-03-19 20:00:35 +0000

    broke out create function

--- app/routes/express-routes.js
+++ app/routes/express-routes.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const db = require('../../config/db.js');
 mongoose.connect(db.url);
 
-const Unicode = require('../schemas/unicode');
+const createUnicode = require('../schema-helpers/create')
 
 module.exports = (router) => {
   router.use((req, res, next) => {
@@ -18,53 +18,7 @@ module.exports = (router) => {
 
   router.route('/unicodes')
     .post((req, res) => {
-      let c = req.body;
-      function allCells () {
-        let content = []
-        for(let i = 0; i < c.code.length; i += 6) {
-          let cells = [{
-            code: c.code[i],
-            character: c.char[i],
-            name: c.name[i],
-          },{
-            code: c.code[i+1],
-            character: c.char[i+1],
-            name: c.name[i+1],
-          },{
-            code: c.code[i+2],
-            character: c.char[i+2],
-            name: c.name[i+2],
-          },{
-            code: c.code[i+3],
-            character: c.char[i+3],
-            name: c.name[i+3],
-          },{
-            code: c.code[i+4],
-            character: c.char[i+4],
-            name: c.name[i+4],
-          },{
-            code: c.code[i+5],
-            character: c.char[i+5],
-            name: c.name[i+5],
-          }]
-
-          content.push({cells: cells})
-        }
-
-        for (let j = 0; j < c.rowTitle.length; j++) {
-          Object.assign(content[j], {row: c.rowTitle[j]});
-        }
-
-        return content
-      }
-
-      const unicode = new Unicode ({
-        language: c.language,
-        url: c.url,
-        column: c.columnTitle.map(obj => Object.assign({}, {name: obj})),
-        content: allCells()
-      });
-
+        let unicode = createUnicode(req.body);
         unicode.save((err) => {
         if (err) { res.send(err) };
         res.json({ message: 'Unicode created!'})
---
+++ app/schema-helpers/create.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const Unicode = require('../schemas/unicode');
+
+function allCells (input) {
+  let content = []
+  for(let i = 0; i < input.code.length; i += 6) {
+    let cells = [{
+      code: input.code[i],
+      character: input.char[i],
+      name: input.name[i],
+    },{
+      code: input.code[i+1],
+      character: input.char[i+1],
+      name: input.name[i+1],
+    },{
+      code: input.code[i+2],
+      character: input.char[i+2],
+      name: input.name[i+2],
+    },{
+      code: input.code[i+3],
+      character: input.char[i+3],
+      name: input.name[i+3],
+    },{
+      code: input.code[i+4],
+      character: input.char[i+4],
+      name: input.name[i+4],
+    },{
+      code: input.code[i+5],
+      character: input.char[i+5],
+      name: input.name[i+5],
+    }]
+
+    content.push({cells: cells})
+  }
+
+  for (let j = 0; j < input.rowTitle.length; j++) {
+    Object.assign(content[j], {row: input.rowTitle[j]});
+  }
+
+  return content
+}
+
+
+module.exports = (input) => {
+  return new Unicode ({
+    language: input.language,
+    url: input.url,
+    column: input.columnTitle.map(obj => Object.assign({}, {name: obj})),
+    content: allCells(input)
+  });
+}
